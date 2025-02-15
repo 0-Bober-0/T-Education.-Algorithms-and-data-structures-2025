664b416999677c70fb6efe87198200ffe5ca712b
Alexandr Daev <97379141+0-Bober-0@users.noreply.github.com>
2025-02-15 22:57:56 +0300

    Update TaskI.java

--- src/main/java/task1/TaskI.java
+++ src/main/java/task1/TaskI.java
@@ -18,8 +18,8 @@ public class TaskI {
 
             int[] arr = new int[n];
 
-            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(input[i]);
-
+            for (int i = 0; i < n; i++) 
+                arr[i] = Integer.parseInt(input[i]);
 
             Set<Integer> arrSet = new HashSet<>();
             int current = n;
